f3f96a3c26f5fb51e7ab5d11f0081cfcc379dba6
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-03-26 15:50:53 +0300

    add list categories

--- src/components/Wrapper/Header/NavBar/NavBar.jsx
+++ src/components/Wrapper/Header/NavBar/NavBar.jsx
@@ -1,19 +1,76 @@
-import React from 'react';
-import { CategoriesContainer, Container, ListContainer } from './NavBarStyled';
+import React, { useState } from 'react';
+import {
+  CategoriesContainer,
+  Container,
+  ItemCategories,
+  ListCategories,
+  ListCategoriesContainer,
+  ListContainer,
+} from './NavBarStyled';
+
+const list = [
+  {
+    id: 1,
+    category: 'Tablet as a laptop',
+  },
+  {
+    id: 2,
+    category: 'Wireless headphones',
+  },
+  {
+    id: 3,
+    category: 'Tablet as a laptop',
+  },
+  {
+    id: 4,
+    category: 'Wireless headphones',
+  },
+  {
+    id: 5,
+    category: 'Tablet as a laptop',
+  },
+  {
+    id: 6,
+    category: 'Wireless headphones',
+  },
+  {
+    id: 7,
+    category: 'Tablet as a laptop',
+  },
+  {
+    id: 8,
+    category: 'Tablet as a laptop',
+  },
+];
 
 const NavBar = () => {
+  const [showCategories, setShowCategories] = useState(false);
+  const handleClick = () => {
+    setShowCategories((prev) => !prev);
+  };
   return (
-    <Container>
-      <CategoriesContainer>
-        <p>Browse categories</p>
-      </CategoriesContainer>
-      <ListContainer>
-        <p>Home</p>
-        <p>Catalog</p>
-        <p>Pages</p>
-        <p>About us</p>
-      </ListContainer>
-    </Container>
+    <>
+      <Container>
+        <CategoriesContainer onClick={handleClick}>
+          <p>Browse categories</p>
+        </CategoriesContainer>
+        <ListContainer>
+          <p>Home</p>
+          <p>Catalog</p>
+          <p>Pages</p>
+          <p>About us</p>
+        </ListContainer>
+      </Container>
+      <ListCategoriesContainer showCategories={showCategories}>
+        <ListCategories>
+          {list.map((item) => (
+            <ItemCategories key={item.id}>
+              <p>{item.category}</p>
+            </ItemCategories>
+          ))}
+        </ListCategories>
+      </ListCategoriesContainer>
+    </>
   );
 };
 
