d27b5342e89f6586a41d66d9fce3132e826a9097
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-03-31 13:37:48 +0300

    change banner and do adaptive benefits

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import baner from '../../assets/images/pexels-nao-triponez-129208 1.png';
-import { ButtonStyled, Container, ContainerInfo } from './BannerStyled';
+import ukr from '../../assets/images/Ukraine.jpg';
+import { Container } from './BannerStyled';
 
 const Banner = () => {
   return (
     <Container>
       <img
-        src={baner}
+        src={ukr}
         alt="banner"
       />
-      <ContainerInfo>
-        <ButtonStyled type="button">New laptop </ButtonStyled>
-        <div>
-          <p>Sale up to 50% off</p>
-          <p>12 inch hd display</p>
-        </div>
-        <ButtonStyled type="button">Shop now</ButtonStyled>
-      </ContainerInfo>
     </Container>
   );
 };
